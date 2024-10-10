cfe1d1d4da95840aeaecc514c136e80db8981666
baudoin <baudoinvicbolingo@gmail.com>
2024-10-10 15:20:13 +0200

    coffe app

---
+++ Screens/Payementscreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+
+
+const Payementscreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the Home Screen!</Text>
+      {/* You can add more UI elements here */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Payementscreen
