e65efcd131708d7a446774980e94acedcca62e23
baudoin <baudoinvicbolingo@gmail.com>
2024-10-10 16:31:27 +0200

    fixing the homescreen

--- Screens/Homescreen.js
+++ Screens/Homescreen.js
@@ -1,3 +1,6 @@
+
+
+
 import React from "react";
 import {
   View,
@@ -6,12 +9,63 @@ import {
   Image,
   TouchableOpacity,
   StyleSheet,
+  ScrollView,
 } from "react-native";
 
 const HomeScreen = () => {
+  const coffeeItems = [
+    {
+      id: 1,
+      name: "Espresso",
+      description: "With Steamed Milk",
+      price: "$5.00",
+      image:
+        "https://media.istockphoto.com/id/1484464555/photo/sip-savor-and-enjoy-a-perfectly-crafted-cappuccino-with-a-beautiful-twist.jpg?s=612x612&w=0&k=20&c=PwLcZk-OoqdlDMBCX74Eaehw_M0EmwUIELckrt9vEjQ=",
+    },
+    {
+      id: 2,
+      name: "Espresso",
+      description: "With Steamed Milk",
+      price: "$5.00",
+      image:
+        "https://media.istockphoto.com/id/1496859162/photo/coffee-cup-latte-art-on-wooden-table-cafe-menu.jpg?s=612x612&w=0&k=20&c=bDz1TO4e0NfMOgUYpAAWWDzba3msUg2cSLSzXV6iZVk=",
+    },
+    {
+      id: 3,
+      name: "Coffee",
+      description: "Coffee beans",
+      price: "$4.00",
+      image:
+        "https://media.istockphoto.com/id/1396456676/photo/coffee-grains-seamless-pattern.jpg?s=612x612&w=0&k=20&c=OGu1cKKaIN3GCS8ByR-QfaZ6kH3aoTjkRQn3fjXiQw0=",
+    },
+    {
+      id: 4,
+      name: "Coffee",
+      description: "Coffee beans",
+      price: "$4.00",
+      image:
+        "https://media.istockphoto.com/id/1039706048/photo/coffee-beans.jpg?s=612x612&w=0&k=20&c=HAWwM-GrBV1vxBrxQmpT1WQrlTg9L5Yr9J4NyPSzWW0=",
+    },
+  ];
+
+  const renderCoffeeItem = (item) => (
+    <View key={item.id} style={styles.coffeeItemContainer}>
+      <Image source={{ uri: item.image }} style={styles.coffeeImage} />
+      <View style={styles.coffeeDetails}>
+        <Text style={styles.coffeeDescription}>{item.name}</Text>
+        <Text style={styles.desc}>{item.description}</Text>
+        <View style={styles.priceAndAddContainer}>
+          <Text style={styles.coffeePrice}>{item.price}</Text>
+          <TouchableOpacity style={styles.addButton}>
+            <Text style={styles.addButtonText}>+</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+
   return (
-    <View style={styles.container}>
-   
+    <ScrollView style={styles.container}>
       <Text style={styles.title}>Find the best{"\n"}coffee for you.</Text>
 
       <TextInput
@@ -20,96 +74,30 @@ const HomeScreen = () => {
         placeholderTextColor="#888"
       />
 
-    
-      <View style={styles.coffeeItem}>
-        <Image
-          source={{
-            uri: "https://media.istockphoto.com/id/1484464555/photo/sip-savor-and-enjoy-a-perfectly-crafted-cappuccino-with-a-beautiful-twist.jpg?s=612x612&w=0&k=20&c=PwLcZk-OoqdlDMBCX74Eaehw_M0EmwUIELckrt9vEjQ=",
-          }} 
-          style={styles.coffeeImage}
-        />
-       
-        <View style={styles.coffeeDetails}>
-          <Text style={styles.coffeeDescription}>Espresso</Text>
-          <Text style={styles.desc}>With Steamed Milk</Text>
-          <Text style={styles.coffeePrice}>$5.00</Text>
+      <View style={styles.coffeeGrid}>
+        <View style={styles.coffeeRow}>
+          {renderCoffeeItem(coffeeItems[0])}
+          {renderCoffeeItem(coffeeItems[1])}
         </View>
-      
-        <TouchableOpacity style={styles.addButton}>
-          <Text style={styles.addButtonText}>+</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.coffeeItem}>
-        <Image
-          source={{
-            uri: "https://media.istockphoto.com/id/1496859162/photo/coffee-cup-latte-art-on-wooden-table-cafe-menu.jpg?s=612x612&w=0&k=20&c=bDz1TO4e0NfMOgUYpAAWWDzba3msUg2cSLSzXV6iZVk=",
-          }} 
-          style={styles.coffeeImage}
-        />
-       
-        <View style={styles.coffeeDetails}>
-          <Text style={styles.coffeeDescription}>Espresso</Text>
-          <Text style={styles.desc}>With Steamed Milk</Text>
-          <Text style={styles.coffeePrice}>$5.00</Text>
+        <View style={styles.coffeeRow}>
+          {renderCoffeeItem(coffeeItems[2])}
+          {renderCoffeeItem(coffeeItems[3])}
         </View>
-      
-        <TouchableOpacity style={styles.addButton}>
-          <Text style={styles.addButtonText}>+</Text>
-        </TouchableOpacity>
       </View>
-
-      <View style={styles.coffeeItem}>
-        <Image
-          source={{
-            uri: "https://media.istockphoto.com/id/1396456676/photo/coffee-grains-seamless-pattern.jpg?s=612x612&w=0&k=20&c=OGu1cKKaIN3GCS8ByR-QfaZ6kH3aoTjkRQn3fjXiQw0=",
-          }} 
-          style={styles.coffeeImage}
-        />
-      
-        <View style={styles.coffeeDetails}>
-          <Text style={styles.coffeeDescription}>Espresso</Text>
-          <Text style={styles.desc}>With Steamed Milk</Text>
-          <Text style={styles.coffeePrice}>$5.00</Text>
-        </View>
-       
-        <TouchableOpacity style={styles.addButton}>
-          <Text style={styles.addButtonText}>+</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.coffeeItem}>
-        <Image
-          source={{
-            uri: "https://media.istockphoto.com/id/1039706048/photo/coffee-beans.jpg?s=612x612&w=0&k=20&c=HAWwM-GrBV1vxBrxQmpT1WQrlTg9L5Yr9J4NyPSzWW0=",
-          }} 
-          style={styles.coffeeImage}
-        />
-    
-        <View style={styles.coffeeDetails}>
-          <Text style={styles.coffeeDescription}>Espresso</Text>
-          <Text style={styles.desc}>With Steamed Milk</Text>
-          <Text style={styles.coffeePrice}>$5.00</Text>
-        </View>
-      
-        <TouchableOpacity style={styles.addButton}>
-          <Text style={styles.addButtonText}>+</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000", 
+    backgroundColor: "#000",
     padding: 20,
   },
   title: {
     fontSize: 32,
     fontWeight: "bold",
-    color: "#fff", 
+    color: "#fff",
     marginBottom: 20,
   },
   searchBar: {
@@ -118,33 +106,42 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
     paddingHorizontal: 10,
-    color: "#fff", 
+    color: "#fff",
     marginBottom: 30,
   },
-
-  desc: {
-    color: "white",
-    fontSize: 12,
+  coffeeGrid: {
+    flexDirection: "column",
   },
-
-  coffeeItem: {
+  coffeeRow: {
     flexDirection: "row",
-    alignItems: "center",
+    justifyContent: "space-between",
     marginBottom: 20,
   },
-  coffeeImage: {
-    width: 200,
-    height: 100,
-    borderRadius: 10,
+  coffeeItemContainer: {
+    width: "48%",
   },
-
+  coffeeImage: {
+    width: "100%",
+    height: 150,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
   coffeeDetails: {
-    flex: 1,
-    marginLeft: 20,
+    marginBottom: 10,
   },
   coffeeDescription: {
     fontSize: 18,
     color: "#fff",
+  },
+  desc: {
+    color: "white",
+    fontSize: 12,
+    marginBottom: 5,
+  },
+  priceAndAddContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   coffeePrice: {
     fontSize: 16,
@@ -152,12 +149,13 @@ const styles = StyleSheet.create({
   },
   addButton: {
     backgroundColor: "#D17842",
-    padding: 10,
-    borderRadius: 50,
+    padding: 8,
+    borderRadius: 8,
   },
   addButtonText: {
-    fontSize: 18,
+    fontSize: 16,
     color: "#FFF",
+    fontWeight: "bold",
   },
 });
 
