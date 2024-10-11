91552e72e4e21040cd0f271651a9007503569252
baudoin <baudoinvicbolingo@gmail.com>
2024-10-11 14:49:23 +0200

    added new icons

--- Store/Store.js
+++ Store/Store.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Store = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-around",
+        padding: 10,
+      }}
+    >
+      <Icon name="home" size={30} color="#fff" />
+      <Icon name="shopping-cart" size={30} color="#fff" />
+      <Icon name="heart" size={30} color="#fff" />
+      <Icon name="bell" size={30} color="#fff" />
+    </View>
+  );
 }
 
 export default Store
