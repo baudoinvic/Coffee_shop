5489944ffc233cee31bd07620b3acea1dab2e6e8
baudoin <baudoinvicbolingo@gmail.com>
2024-10-11 15:42:15 +0200

    fixed icons

--- Store/Store.js
+++ Store/Store.js
@@ -1,19 +1,30 @@
+
+
+
 import React from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
 const Store = () => {
   return (
     <View style={styles.container}>
-      <Icon name="home" size={30} color="#D17842" style={styles.icon} />
-      <Icon
-        name="shopping-cart"
-        size={30}
-        color="#4E5053"
-        style={styles.icon}
-      />
-       
-      <Icon name="heart" size={30} color="#4E5053" style={styles.icon} />
-      <Icon name="bell" size={30} color="#4E5053" style={styles.icon} />
+      <TouchableOpacity>
+        <Icon name="home" size={30} color="#D17842" style={styles.icon} />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Icon
+          name="shopping-cart"
+          size={30}
+          color="#4E5053"
+          style={styles.icon}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Icon name="heart" size={30} color="#4E5053" style={styles.icon} />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Icon name="bell" size={30} color="#4E5053" style={styles.icon} />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -25,9 +36,6 @@ const styles = StyleSheet.create({
     padding: 10,
     marginTop: -30,
   },
-  Icon: {
-    cursor: "pointer",
-  }
 });
 
 export default Store;
