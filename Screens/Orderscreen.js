1d30acfa3f359c0818ccec9d01e71aafb917be04
baudoin <baudoinvicbolingo@gmail.com>
2024-10-14 12:43:42 +0200

    fixing order history screen

--- Screens/Orderscreen.js
+++ Screens/Orderscreen.js
@@ -16,6 +16,52 @@ const Orderscreen = () => {
           <Text>$74.40</Text>
         </View>
       </View>
+
+      <View style={styles.cartItem}>
+        <View style={styles.fix}>
+          <Image
+            source={{
+              uri: "https://media.istockphoto.com/id/1484464555/photo/sip-savor-and-enjoy-a-perfectly-crafted-cappuccino-with-a-beautiful-twist.jpg?s=612x612&w=0&k=20&c=PwLcZk-OoqdlDMBCX74Eaehw_M0EmwUIELckrt9vEjQ=",
+            }}
+            style={styles.image}
+          />
+          <View style={styles.details}>
+            <Text style={styles.textcart}>Cappuccino</Text>
+            <Text style={styles.small}>With Steamed Milk</Text>
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Medium Roasted</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.sizePriceQuantity}>
+          {["S", "M", "L"].map((size, index) => (
+            <View key={index} style={styles.row}>
+              <TouchableOpacity
+                style={[
+                  styles.sizeButton,
+                  size === "S"
+                    ? styles.sizeS
+                    : size === "M"
+                    ? styles.sizeM
+                    : styles.sizeL,
+                ]}
+              >
+                <Text style={styles.sizeText}>{size}</Text>
+              </TouchableOpacity>
+              <Text style={styles.price}>$4.20</Text>
+              <TouchableOpacity style={styles.adjustButton}>
+                <Text style={styles.adjustButtonText}>-</Text>
+              </TouchableOpacity>
+              <Text style={styles.quantity}>1</Text>
+              <TouchableOpacity style={styles.adjustButton}>
+                <Text style={styles.adjustButtonText}>+</Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </View>
+      </View>
     </View>
   );
 }
