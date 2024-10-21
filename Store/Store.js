1431df93987d99a81a2daa8f1d615e551b650fa2
baudoin <baudoinvicbolingo@gmail.com>
2024-10-21 12:59:32 +0200

    fixing

--- Store/Store.js
+++ Store/Store.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const Store = () => {
