08113fee93cbe4a7270af5784024582b0a93055f
baudoin <baudoinvicbolingo@gmail.com>
2024-10-22 12:41:13 +0200

    debuging

--- Screens/Orderscreen.js
+++ Screens/Orderscreen.js
@@ -1,50 +1,99 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-
-const Orderscreen = () => {
+const Cartscreen = () => {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <Image
-        source={{
-          uri: "https://media.istockphoto.com/id/1484464555/photo/sip-savor-and-enjoy-a-perfectly-crafted-cappuccino-with-a-beautiful-twist.jpg?s=612x612&w=0&k=20&c=PwLcZk-OoqdlDMBCX74Eaehw_M0EmwUIELckrt9vEjQ=",
-        }}
-        style={styles.image}
-      />
-      <Text style={styles.text}>Description</Text>
-      <Text style={styles.desc}>
-        Cappuccino is a latte made with more foam than steamed milk, often with
-        a sprinkle of cocoa powder or cinnamon on top.
-      </Text>
+      <View style={styles.cartItem}>
+        <View style={styles.fix}>
+          <Image
+            source={{
+              uri: "https://media.istockphoto.com/id/1484464555/photo/sip-savor-and-enjoy-a-perfectly-crafted-cappuccino-with-a-beautiful-twist.jpg?s=612x612&w=0&k=20&c=PwLcZk-OoqdlDMBCX74Eaehw_M0EmwUIELckrt9vEjQ=",
+            }}
+            style={styles.image}
+          />
+          <View style={styles.details}>
+            <Text style={styles.textcart}>Cappuccino</Text>
+            <Text style={styles.small}>With Steamed Milk</Text>
 
-      <Text style={styles.size}>Size</Text>
-      <View style={styles.medium}>
-        <View style={styles.firstmedium}>
-          <TouchableOpacity style={styles.big}>
-            <Text style={styles.free}>S</Text>
-          </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Medium Roasted</Text>
+            </TouchableOpacity>
+          </View>
         </View>
 
-        <View style={styles.secondmedium}>
-          <TouchableOpacity style={styles.big}>
-            <Text style={styles.third}>M</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.thirdmedium}>
-          <TouchableOpacity style={styles.big}>
-            <Text style={styles.third}>L</Text>
-          </TouchableOpacity>
+        <View style={styles.sizePriceQuantity}>
+          {["S", "M", "L"].map((size, index) => (
+            <View key={index} style={styles.row}>
+              <TouchableOpacity
+                style={[
+                  styles.sizeButton,
+                  size === "S"
+                    ? styles.sizeS
+                    : size === "M"
+                    ? styles.sizeM
+                    : styles.sizeL,
+                ]}
+              >
+                <Text style={styles.sizeText}>{size}</Text>
+              </TouchableOpacity>
+              <Text style={styles.price}>$4.20</Text>
+              <TouchableOpacity style={styles.adjustButton}>
+                <Text style={styles.adjustButtonText}>-</Text>
+              </TouchableOpacity>
+              <Text style={styles.quantity}>1</Text>
+              <TouchableOpacity style={styles.adjustButton}>
+                <Text style={styles.adjustButtonText}>+</Text>
+              </TouchableOpacity>
+            </View>
+          ))}
         </View>
       </View>
-      <View style={styles.cart}>
-        <Text style={styles.cartprice}>$4.50</Text>
 
+      <View style={styles.cartItem}>
+        <View style={styles.fix}>
+          <Image
+            source={{
+              uri: "https://media.istockphoto.com/id/1496859162/photo/coffee-cup-latte-art-on-wooden-table-cafe-menu.jpg?s=612x612&w=0&k=20&c=bDz1TO4e0NfMOgUYpAAWWDzba3msUg2cSLSzXV6iZVk=",
+            }}
+            style={styles.image}
+          />
+          <View style={styles.details}>
+            <Text style={styles.textcart}>Cappuccino</Text>
+            <Text style={styles.small}>With Steamed Milk</Text>
+
+            <View style={styles.display}>
+              <TouchableOpacity style={styles.medium}>
+                <Text style={styles.Mediumtext}>M</Text>
+              </TouchableOpacity>
+              <Text style={styles.simple}>$6.20</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.sizePriceQuantity}>
+          {["S", "M", "L"].map((size, index) => (
+            <View key={index} style={styles.row}>
+              <TouchableOpacity style={styles.adjustButton}>
+                <Text style={styles.adjustButtonText}>-</Text>
+              </TouchableOpacity>
+              <Text style={styles.quantity}>1</Text>
+              <TouchableOpacity style={styles.adjustButton}>
+                <Text style={styles.adjustButtonText}>+</Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </View>
+      </View>
+      <View style={styles.total}>
+        <Text style={styles.totalText}>Total price $6.20</Text>
         <TouchableOpacity
           style={styles.cartbutton}
-          onPress={() => navigation.navigate("Cart")}
+          onPress={() => navigation.navigate("Payment")}
         >
-          <Text style={styles.carttext}>Add to Cart</Text>
+          <Text style={styles.carttext}>Pay Now</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -54,79 +103,136 @@ const Orderscreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "black",
+    padding: 20,
+  },
+  cartItem: {
+    backgroundColor: "#262B33",
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 15,
+  },
+  fix: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  details: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  textcart: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  small: {
+    color: "#B0B0B0",
+    fontSize: 14,
+  },
+  button: {
+    alignSelf: "flex-start",
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    backgroundColor: "#141921",
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 14,
+  },
+  sizePriceQuantity: {
+    marginTop: 15,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  sizeButton: {
+    width: 30,
+    height: 30,
+    borderRadius: 5,
     justifyContent: "center",
     alignItems: "center",
-    alignItems: "flex-start",
-    backgroundColor: "black",
   },
-
-  desc: {
-    color: "gray",
-    marginTop: 20,
+  sizeS: {
+    backgroundColor: "#1F2429",
   },
-
-  size: {
-    marginTop: 10,
+  sizeM: {
+    backgroundColor: "#1F2429",
+  },
+  sizeL: {
+    backgroundColor: "#1F2429",
+  },
+  sizeText: {
     color: "white",
+    fontWeight: "bold",
   },
-
-  big: {
-    borderWidth: 2,
-    borderStyle: "solid",
-    borderColor: "#D17842",
-    padding: 15,
-    marginTop: 10,
-    backgroundColor: "#141921",
-  },
-
-  medium: {
-    display: "flex",
-    flexDirection: "row",
-  },
-
-  thirdmedium: {
-    marginLeft: 30,
-    width: 100,
-  },
-
-  free: {
+  price: {
     color: "white",
-    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold",
   },
-
-  third: {
-    textAlign: "center",
+  adjustButton: {
+    backgroundColor: "#D17842",
+    width: 30,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  adjustButtonText: {
     color: "white",
-  },
-
-  firstmedium: {
-    width: 100,
-  },
-
-  secondmedium: {
-    marginLeft: 30,
-    width: 100,
-  },
-
-  image: {
-    width: 400,
-    height: 500,
-    borderRadius: 10,
-    marginTop: -180,
-  },
-  text: {
-    marginTop: 20,
     fontSize: 18,
-    color: "white",
   },
-
+  quantity: {
+    color: "white",
+    fontSize: 16,
+    marginHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#D17842",
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    borderRadius: 5,
+  },
+  medium: {
+    backgroundColor: "#1F2429",
+    padding: 15,
+    width: 100,
+    marginTop: 10,
+    borderRadius: 20,
+  },
+  Mediumtext: {
+    color: "white",
+    textAlign: "center",
+  },
+  display: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  simple: {
+    color: "white",
+    marginTop: 10,
+    fontSize: 22,
+    fontWeight: "bold",
+    marginLeft: 20,
+  },
   cartbutton: {
     backgroundColor: "#D17842",
     padding: 20,
     width: 220,
     borderRadius: 10,
-    marginLeft: 80,
-    marginTop: -10,
+    marginLeft: 10,
+    marginTop: 10,
   },
 
   carttext: {
@@ -134,18 +240,15 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
   },
-  cart: {
-    flexDirection: "row",
-    marginTop: 50,
-    alignItems: "center",
-  },
 
-  cartprice: {
+  totalText: {
     color: "white",
-    marginTop: 10,
-    fontSize: 20,
-    marginLeft: 5,
+    fontSize: "15",
+  },
+  total: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
 
-export default Orderscreen;
+export default Cartscreen;
