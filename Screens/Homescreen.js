8ada3506f62349b5dffa9540039827349ad3eeeb
baudoin <baudoinvicbolingo@gmail.com>
2024-11-30 16:55:29 +0200

    fixing home page

--- Screens/Homescreen.js
+++ Screens/Homescreen.js
@@ -59,8 +59,6 @@ const HomeScreen = () => {
     }
   };
   
-
-  
   const renderCoffeeItem = (item) => (
     <View key={item.id} style={styles.coffeeItemContainer}>
       <Image source={{ uri: item.image }} style={styles.coffeeImage} />
